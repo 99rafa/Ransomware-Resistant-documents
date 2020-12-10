1f196a897d6e887771e9faa4d64ce0507c9f9d26
MateusLM <mateuslibanomonteiro@gmail.com>
2020-12-10 23:07:47 +0000

    certs

--- SirsRansomware/run_scripts/backup.sh
+++ SirsRansomware/run_scripts/backup.sh
@@ -4,4 +4,4 @@ LOCALPATH=$(pwd)
 
 # Backup initialization
 
-mvn exec:java -Dexec.mainClass="server.BackupServer" -Dexec.args="1 1 2181 192.168.0.100 8086 192.168.0.200 $LOCALPATH/src/assets/certs/server1.pem $LOCALPATH/src/assets/certs/server1.key $LOCALPATH/src/assets/certs/ca.pem"
+mvn exec:java -Dexec.mainClass="server.BackupServer" -Dexec.args="1 1 2181 192.168.0.100 8086 192.168.0.200 $LOCALPATH/src/assets/certs/server.pem $LOCALPATH/src/assets/certs/server.key $LOCALPATH/src/assets/certs/ca.pem"
--- SirsRansomware/run_scripts/server.sh
+++ SirsRansomware/run_scripts/server.sh
@@ -4,4 +4,4 @@ LOCALPATH=$(pwd)
 
 # Server initialization
 
-mvn exec:java -Dexec.mainClass="server.Server" -Dexec.args="mateus pass 2181 192.168.0.100 8085 192.168.0.100 $LOCALPATH/src/assets/certs/server0.pem $LOCALPATH/src/assets/certs/server0.key $LOCALPATH/src/assets/certs/ca.pem"
+mvn exec:java -Dexec.mainClass="server.Server" -Dexec.args="mateus pass 2181 192.168.0.100 8085 192.168.0.100 $LOCALPATH/src/assets/certs/server.pem $LOCALPATH/src/assets/certs/server.key $LOCALPATH/src/assets/certs/ca.pem"
