8b2384bf5d6f0d619e19d5080c41d12485b0db65
afonsoparedes <afonso.paredes@hotmail.com>
2020-12-10 17:03:14 +0000

    Updated scripts

--- SirsRansomware/run_scripts/backup1.sh
+++ SirsRansomware/run_scripts/backup1.sh
@@ -4,4 +4,4 @@ LOCALPATH=$(pwd)
 
 # Backup 1 initialization
 
-mvn exec:java -Dexec.mainClass="server.BackupServer" -Dexec.args="1 2 2181 192.168.0.100 8086 192.168.0.201 $LOCALPATH/src/assets/certs/server1.pem $LOCALPATH/src/assets/certs/server1.key $LOCALPATH/src/assets/certs/ca.pem"
+mvn exec:java -Dexec.mainClass="server.BackupServer" -Dexec.args="1 2 2181 192.168.0.100 8086 192.168.0.201 $LOCALPATH/src/assets/certs/client.pem $LOCALPATH/src/assets/certs/client.key $LOCALPATH/src/assets/certs/ca.pem"
